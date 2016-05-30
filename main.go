8d89b247b36c77154f4e5396fddd6521fd27017c
nickchou <nick.chou@live.com>
2016-05-30 23:48:55 +0800

    调整404样式

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 )
 
 func main() {
-	var port string =  ":10000"
-	
+	var port string = ":10000"
+
 	http.Handle("/css/", http.FileServer(http.Dir("static")))
 	http.Handle("/js/", http.FileServer(http.Dir("static")))
 	http.Handle("/image/", http.FileServer(http.Dir("static")))
-	
+
 	//reg handleFunc,see route.go
-	http.HandleFunc("/index",Index)
-	http.HandleFunc("/week/*",Week)
-	http.HandleFunc("/",DefHandle)
+	http.HandleFunc("/index", Index)
+	http.HandleFunc("/week", Week)
+	http.HandleFunc("/", DefHandle)
 	//console
-	fmt.Println("listen port"+port)
+	fmt.Println("listen port" + port)
 	//lister port
 	http.ListenAndServe(port, nil)
 }
